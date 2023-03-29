bd0a35460efc22b1b1402c85cc534325b9ab4006
Emin ALTUN <114393466+altun93@users.noreply.github.com>
2023-03-29 12:29:37 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import UseEffectFetchData from "./useEffect-fetch/useEffect-fetch.data";
+
+function App() {
+  return (
+    <div className="container">
+      <UseEffectFetchData />
+    </div>
+  );
+}
+
+export default App;
